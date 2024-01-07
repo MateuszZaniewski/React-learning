fedc5243336245ca76c6f4580acf1a1aa7ef0dc5
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2024-01-07 18:39:49 +0100

    working on sticky notes

--- src/components/NoteForm.jsx
+++ src/components/NoteForm.jsx
@@ -1,7 +1,95 @@
-export default function NoteForm() {
+import { useState } from "react";
+
+export default function NoteForm({ onSubmit, setNoteCreation, noteCreation }) {
+  const [color, setColor] = useState("bg-green-300");
+  const [tittle, setTittle] = useState("");
+  const [text, setText] = useState("");
+
+  const handleSelectChange = (e) => {
+    const desiredColor = e.target.dataset.color;
+    setColor(desiredColor);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onSubmit([tittle, text, color]);
+    setTittle("");
+    setText("");
+    setColor("bg-green-300");
+    setNoteCreation(!noteCreation);
+  };
+
+  const handleTitleChange = (e) => {
+    setTittle(e.target.value);
+  };
+
+  const handleTextChange = (e) => {
+    setText(e.target.value);
+  };
+
   return (
-    <div className="h-20 w-20 border">
-      <form className=""></form>
+    <div
+      className={`h-[60vmin] w-[450px] mt-5 px-4 pb-4 border rounded-lg ${color}`}
+    >
+      <form onSubmit={handleSubmit} className="flex flex-col">
+        <div className="w-full flex justify-end pt-4">
+          <button
+            className="px-2 py-1 border border-gray-500 bg-white rounded-lg text-xl"
+            type="submit"
+          >
+            Save
+          </button>
+        </div>
+        <label className="text-2xl py-2">Tittle:</label>
+        <input
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              e.preventDefault();
+            }
+          }}
+          value={tittle}
+          onChange={handleTitleChange}
+        ></input>
+        <label className="text-2xl py-2">Text:</label>
+        <textarea
+          value={text}
+          onChange={handleTextChange}
+          className="resize-none h-[30vmin]"
+        ></textarea>
+        <label className="text-2xl py-2">Select color:</label>
+        <div className="flex justify-around">
+          <div
+            data-color={"bg-green-300"}
+            className="h-8 w-12 rounded-lg border border-black bg-green-300"
+            onClick={(e) => handleSelectChange(e)}
+          ></div>
+          <div
+            data-color={"bg-blue-300"}
+            className="h-8 w-12 rounded-lg border border-black bg-blue-300"
+            onClick={(e) => handleSelectChange(e)}
+          ></div>
+          <div
+            data-color={"bg-red-300"}
+            className="h-8 w-12 rounded-lg border border-black bg-red-300"
+            onClick={(e) => handleSelectChange(e)}
+          ></div>
+          <div
+            data-color={"bg-yellow-300"}
+            className="h-8 w-12 rounded-lg border border-black bg-yellow-300"
+            onClick={(e) => handleSelectChange(e)}
+          ></div>
+          <div
+            data-color={"bg-purple-300"}
+            className="h-8 w-12 rounded-lg border border-black bg-purple-300"
+            onClick={(e) => handleSelectChange(e)}
+          ></div>
+          <div
+            data-color={"bg-pink-300"}
+            className="h-8 w-12 rounded-lg border border-black bg-pink-300"
+            onClick={(e) => handleSelectChange(e)}
+          ></div>
+        </div>
+      </form>
     </div>
   );
 }
--- src/components/StickyWallView.jsx
+++ src/components/StickyWallView.jsx
@@ -1,9 +1,15 @@
 import { useState } from "react";
 import NoteForm from "./NoteForm";
+import add from "../assets/add.png";
 
 export default function StickyWallView() {
   const [notes, setNotes] = useState([]);
   const [noteCreation, setNoteCreation] = useState(false);
+
+  const getData = (data) => {
+    setNotes([...notes, data]);
+    console.log(notes);
+  };
 
   const addNote = (title) => {
     const note = title;
@@ -11,35 +17,53 @@ export default function StickyWallView() {
   };
 
   return (
-    <div className="my-6 ">
+    <div className="my-6 w-[450px] ">
       <div className="">
         <h1 className="text-5xl font-bold ">Sticky Notes</h1>
       </div>
-      <div className="pt-8">
-        <button
+      <div
+        onClick={() => setNoteCreation(!noteCreation)}
+        className={`pt-8 gap-2 items-center ${
+          !noteCreation ? "flex" : "hidden"
+        }`}
+      >
+        <img src={add} className="h-12 w-12" />
+        <span className="text-xl">Add note</span>
+      </div>
+      <div
+        className={`pt-8 justify-between items-center ${
+          noteCreation ? "flex" : "hidden"
+        }`}
+      >
+        <span
           onClick={() => setNoteCreation(!noteCreation)}
-          className="px-2 py-1 border rounded-xl border-green-300 bg-green-300 text-xl"
+          className="text-xl"
         >
-          Add new note
-        </button>
+          Cancel
+        </span>
       </div>
       <div className="flex flex-col justify-start overflow-y-auto max-h-[75vmin]">
         <div className={noteCreation ? "block" : "hidden"}>
-          <NoteForm />
+          <NoteForm
+            addNote={addNote}
+            onSubmit={getData}
+            setNoteCreation={setNoteCreation}
+            noteCreation={noteCreation}
+          />
         </div>
 
-        {/* <ul>
+        <ul>
           {notes.map((item, index) => {
             return (
               <div
                 key={index}
                 className={`pt-2 flex justify-start items-center max-w-sm md:max-w-lg lg:max-w-3xl gap-2 pb-2 w-fit`}
               >
-                <span key={index}>{item}</span>
+                <span key={index}>{item[0]}</span>
               </div>
             );
           })}
-        </ul> */}
+        </ul>
       </div>
     </div>
   );
